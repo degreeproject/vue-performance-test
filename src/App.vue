7b6ea6095e74a91ffad7939523290f98931e4948
tiico <ockelberg@gmail.com>
2019-08-30 17:02:09 +0200

    started work on DOM-manipulation test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to an actual App asasasassaasas"/>
+    <domtest></domtest>
+    <result :data="resultData"></result>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import result from './components/Result.vue'
+import domtest from './components/DOM-test.vue'
 
 export default {
   name: 'app',
+    data() {
+      return {
+      resultData: [
+          { name: 'American alligator', location: 'Southeast United States' },
+          { name: 'Chinese alligator', location: 'Eastern China' },
+          { name: 'Spectacled caiman', location: 'Central & South America' },
+          { name: 'Broad-snouted caiman', location: 'South America' },
+          { name: 'Jacaré caiman', location: 'South America' },
+          { name: 'Black caiman', location: 'South America' },
+        ], 
+      }
+  },
   components: {
-    HelloWorld
+    result,
+    domtest
   }
 }
 </script>
---
+++ src/components/DOM-test.vue
@@ -0,0 +1,48 @@
+<template>
+<div id="wrapper">
+<p>Press the button to initiate the DOM-manipulation tests</p>
+<b-form-input id="rowinput" v-model="rows" placeholder="Enter amount of rows"></b-form-input>
+<b-button variant="danger" v-if="renderRows" @click="startTest">Toggle</b-button>
+<b-button variant="success" v-if="!renderRows" @click="startTest">Toggle</b-button>
+<p>Render: {{this.renderRows}}</p>
+  <ul v-if="renderRows">
+    <li v-for="index in Number(rows)" :key="index">
+      {{index}}
+    </li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: "DOM-test",
+  props: {
+  },
+  data(){
+    return {
+      rows: 10,
+      renderRows: false,
+    }
+  },
+  methods: {
+    startTest(){
+      this.renderRows = !this.renderRows;
+    },
+  },
+  computed:{
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  #result{
+    width: 35%;
+  }
+  #rowinput{
+    width: 20%;
+    text-align: center;
+    margin-left: 40%;
+    margin-bottom: 1%;
+  }
+</style>
